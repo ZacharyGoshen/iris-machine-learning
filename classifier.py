7ab44ac9cd2cde55e1e5f4ac46ef3e38303211c6
Zachary Goshen <zachgoshen@gmail.com>
2019-07-02 12:26:18 -0400

    Added preprocess_data.py to split the data into train/test sets

--- classifier.py
+++ classifier.py
@@ -8,19 +8,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import tensorflow as tf
 
-# Read in the Iris data
-iris_df = pd.read_csv("./iris.txt", sep = ",")
-
-# Create additional features that report whether a sample belongs to each species
-iris_df['is_setosa'] = (iris_df['class'] == "Iris-setosa").astype(float)
-iris_df['is_versicolor'] = (iris_df['class'] == "Iris-versicolor").astype(float)
-iris_df['is_virginica'] = (iris_df['class'] == "Iris-virginica").astype(float)
-
-# Create a feature for our future predictions
-iris_df["predicted_class"] = "No prediction"
-
-# Split data into training and test sets
-train_df, test_df = train_test_split(iris_df, test_size = 0.2)
+# Read in the training and test sets
+train_df = pd.read_csv("./iris_train.txt", sep = ",")
+test_df = pd.read_csv("./iris_test.txt", sep = ",")
 
 # Split the train data into groups seperated by label
 train_setosa = train_df.loc[train_df["is_setosa"] == 1.0]
---
+++ preprocess_data.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+# Read in the Iris data
+iris_df = pd.read_csv("./iris.txt", sep = ",")
+
+# Create additional features that report whether a sample belongs to each species
+iris_df['is_setosa'] = (iris_df['class'] == "Iris-setosa").astype(float)
+iris_df['is_versicolor'] = (iris_df['class'] == "Iris-versicolor").astype(float)
+iris_df['is_virginica'] = (iris_df['class'] == "Iris-virginica").astype(float)
+
+# Create a feature for our future predictions
+iris_df["predicted_class"] = "No prediction"
+
+# Split data into training and test sets
+train_df, test_df = train_test_split(iris_df, test_size = 0.2)
+
+# Write the training and test sets to csv files
+train_df.to_csv("./iris_train.txt", index = False)
+test_df.to_csv("./iris_test.txt", index = False)
