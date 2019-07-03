03e903e4949a71cfefd94a8824657d3f873f7282
Zachary Goshen <zachgoshen@gmail.com>
2019-07-03 07:44:53 -0400

    changed iris_plot.png

--- source/vizualize_data.py
+++ source/vizualize_data.py
@@ -17,13 +17,10 @@ iris_df['is_virginica'] = (iris_df['class'] == "Iris-virginica").astype(float)
 # Create a feature for our future predictions
 iris_df["predicted_class"] = "No prediction"
 
-# Split data into training and test sets
-train_df, test_df = train_test_split(iris_df, test_size = 0.2)
-
-# Split the train data into groups seperated by label
-train_setosa = train_df.loc[train_df["is_setosa"] == 1.0]
-train_versicolor = train_df.loc[train_df["is_versicolor"] == 1.0]
-train_virginica = train_df.loc[train_df["is_virginica"] == 1.0]
+# Split the data into groups seperated by label
+setosa = iris_df.loc[iris_df["is_setosa"] == 1.0]
+versicolor = iris_df.loc[iris_df["is_versicolor"] == 1.0]
+virginica = iris_df.loc[iris_df["is_virginica"] == 1.0]
 
 # Create a 4 x 4 figure of subplots for each feature pair
 fig, ax = plt.subplots(4, 4, sharex = "col", sharey = "row")
@@ -54,9 +51,9 @@ for index in range(1, 17):
 	# If subplot does not have the same feature for both its features
 	if (x_var != y_var):
 		# Group the data based on species
-		g1 = (np.array(train_setosa[x_var]), np.array(train_setosa[y_var]))
-		g2 = (np.array(train_versicolor[x_var]), np.array(train_versicolor[y_var]))
-		g3 = (np.array(train_virginica[x_var]), np.array(train_virginica[y_var]))
+		g1 = (np.array(setosa[x_var]), np.array(setosa[y_var]))
+		g2 = (np.array(versicolor[x_var]), np.array(versicolor[y_var]))
+		g3 = (np.array(virginica[x_var]), np.array(virginica[y_var]))
 
 		# Color and label the groups
 		data = (g1, g2, g3)
@@ -66,7 +63,7 @@ for index in range(1, 17):
 		# Plot the data in the subplot
 		for data, color, group in zip(data, colors, groups):
 			x, y = data
-			ax[row, col].plot(y, x, "o", c = color, label = group, markersize = 3)
+			ax[row, col].plot(y, x, "o", c = color, label = group, markersize = 2)
 
 # Show all subplots
 plt.show()
